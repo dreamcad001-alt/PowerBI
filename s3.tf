4fa362f08860f242e733b85bbc484b96106fdc24
Anusha <dreamcad001@gmail.com>
2026-04-10 09:35:12 +0530

    update

--- s3.tf
+++ s3.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 # S3 Bucket
 resource "aws_s3_bucket" "my_bucket" {
-  bucket = var.bucket_name-${var.env}
+  bucket = "${var.bucket_name}-${var.env}"
   acl    = "private"
 
   versioning {
@@ -18,16 +18,16 @@ resource "aws_s3_bucket" "my_bucket" {
 }
 
 # Upload a file to S3
-resource "aws_s3_object" "Upload_data" {
+resource "aws_s3_object" "upload_data" {
   bucket = aws_s3_bucket.my_bucket.id
-  key    = var.s3_key_name
-  source = var.C:\Users\Anusha\Downloads\Orders.xlsx
-  etag   = filemd5(var.C:\Users\Anusha\Downloads\Orders.xlsx)
+  key    = "Orders.xlsx"
+  source = var.local_file_path
+  etag   = filemd5(var.local_file_path)
 }
 
 # IAM Role for Lambda
 resource "aws_iam_role" "lambda_role" {
-  name = "lambda_s3_role"
+  name               = "lambda_s3_role"
   assume_role_policy = data.aws_iam_policy_document.lambda_assume.json
 }
 
@@ -43,7 +43,7 @@ data "aws_iam_policy_document" "lambda_assume" {
 
 # IAM Policy for Lambda to access S3
 resource "aws_iam_role_policy" "lambda_policy" {
-  role = aws_iam_role.lambda_role.id
+  role   = aws_iam_role.lambda_role.id
   policy = data.aws_iam_policy_document.lambda_s3.json
 }
 
