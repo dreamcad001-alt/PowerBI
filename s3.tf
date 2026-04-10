c62ab77b040e02c7d086c2ba75fcdb55f41844b8
Anusha <dreamcad001@gmail.com>
2026-04-10 17:15:49 +0530

    update

--- s3.tf
+++ s3.tf
@@ -1,12 +1,4 @@
 
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
 
 resource "aws_s3_bucket" "my_bucket" {
   bucket = "${var.bucket_name}-${var.env}"
